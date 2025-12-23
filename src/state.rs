93952360e09e12df74a3ee84537c7d997572e528
Edanur Çağlayan <131459118+edanur-caglayann@users.noreply.github.com>
2025-12-23 11:41:14 +0300

    Fix comment formatting in YeniGezegen struct

--- src/state.rs
+++ src/state.rs
@@ -6,7 +6,7 @@ pub struct YeniGezegen{
     pub name:String, 
     pub age: u8, 
     pub uzaklik:u32, 
-    pub yasanabilirlik:u8, // 0-1 olabilir. En küçük veri tipi u8 olduğundan
+    pub yasanabilirlik:u8, // 0-1 olabilir. En küçük veri tipi u8 olduğundan.
     pub kesfedenadres:[u8;32],// Gezegeni keşfeden kişinin cüzdan adresi. 32 bytelık array
     pub onaylandi:u8,
 }
